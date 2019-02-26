b657b91afdca3ec28e9322b23b4bebe89260de9b
Vanessa White <whitevanessa14@gmail.com>
2019-02-25 21:16:31 -0500

    Add new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user is connected');
 
+  socket.emit('newMessage', {
+    from: 'user1',
+    text: 'im hurr',
+    createdAt: 243
+  });
+
+  socket.on('createMessage', (newMessage) => {
+    console.log('createMessage', newMessage);
+  });
+
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
