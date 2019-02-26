883c1e0ac27ecc29d3adb79d5867f206ca42ba96
Vanessa White <whitevanessa14@gmail.com>
2019-02-25 21:23:48 -0500

    Emit new message on server

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'user2',
-    text: 'yup hurr'
-  });
 });
 
 socket.on('disconnect', function() {
